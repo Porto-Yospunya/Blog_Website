18dd9849e0016b3fdf827dfa4973a444c83113a2
Porto-Yospunya <yospunyaporto@gmail.com>
2024-08-22 13:44:11 +0700

    update

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.get('/', async (req, res) => {
 // read pages articles
 app.use('/articles', articleRouter) 
 
-const PORT = process.env.PORT;
+const port = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
